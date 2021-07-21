29558482aec249e398a17349104e2b99650d0c28
STR1Z <weihengjiang64@hotmail.com>
2021-07-20 20:29:44 -0400

    new build

--- .svelte-kit/output/server/app.js
+++ .svelte-kit/output/server/app.js
@@ -227,7 +227,7 @@ var user_hooks = /* @__PURE__ */ Object.freeze({
 });
 const template = ({ head, body }) => '<!DOCTYPE html>\n<html lang="en">\n\n<head>\n    <meta charset="utf-8" />\n    <link rel="icon" href="/favicon.png" />\n    <meta name="viewport" content="width=device-width, initial-scale=1" />\n    <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"\n        rel="stylesheet">\n    ' + head + '\n</head>\n\n<body class="dark:bg-gray-900 dark:text-gray-100">\n    <div id="app">' + body + "</div>\n</body>\n\n</html>";
 let options = null;
-const default_settings = { paths: { "base": "/dtc-website", "assets": "/dtc-website" } };
+const default_settings = { paths: { "base": "", "assets": "/." } };
 function init(settings = default_settings) {
   set_paths(settings.paths);
   set_prerendering(settings.prerendering || false);
@@ -235,13 +235,13 @@ function init(settings = default_settings) {
     amp: false,
     dev: false,
     entry: {
-      file: "/dtc-website/_app/start-a2a08690.js",
-      css: ["/dtc-website/_app/assets/start-9aa571ba.css"],
-      js: ["/dtc-website/_app/start-a2a08690.js", "/dtc-website/_app/chunks/vendor-2f7a998b.js", "/dtc-website/_app/chunks/singletons-bb9012b7.js"]
+      file: "/./_app/start-a2a08690.js",
+      css: ["/./_app/assets/start-9aa571ba.css"],
+      js: ["/./_app/start-a2a08690.js", "/./_app/chunks/vendor-2f7a998b.js", "/./_app/chunks/singletons-bb9012b7.js"]
     },
     fetched: void 0,
     floc: false,
-    get_component_path: (id2) => "/dtc-website/_app/" + entry_lookup[id2],
+    get_component_path: (id2) => "/./_app/" + entry_lookup[id2],
     get_stack: (error2) => String(error2),
     handle_error: (error2) => {
       if (error2.frame) {
@@ -337,7 +337,7 @@ const module_lookup = {
     return quote;
   })
 };
-const metadata_lookup = { "src/routes/__layout.svelte": { "entry": "/dtc-website/_app/pages/__layout.svelte-fd551ceb.js", "css": ["/dtc-website/_app/assets/pages/__layout.svelte-2c13b87e.css"], "js": ["/dtc-website/_app/pages/__layout.svelte-fd551ceb.js", "/dtc-website/_app/chunks/vendor-2f7a998b.js", "/dtc-website/_app/chunks/links-1a771544.js", "/dtc-website/_app/chunks/LinkButton-d31108c8.js"], "styles": null }, ".svelte-kit/build/components/error.svelte": { "entry": "/dtc-website/_app/error.svelte-e9da64e0.js", "css": [], "js": ["/dtc-website/_app/error.svelte-e9da64e0.js", "/dtc-website/_app/chunks/vendor-2f7a998b.js"], "styles": null }, "src/routes/index.svelte": { "entry": "/dtc-website/_app/pages/index.svelte-f41b6e2b.js", "css": [], "js": ["/dtc-website/_app/pages/index.svelte-f41b6e2b.js", "/dtc-website/_app/chunks/vendor-2f7a998b.js", "/dtc-website/_app/chunks/LinkButton-d31108c8.js", "/dtc-website/_app/chunks/links-1a771544.js", "/dtc-website/_app/chunks/services-eeb3c769.js", "/dtc-website/_app/chunks/people-c7c3e1c6.js"], "styles": null }, "src/routes/quote-requested.svelte": { "entry": "/dtc-website/_app/pages/quote-requested.svelte-248181ae.js", "css": [], "js": ["/dtc-website/_app/pages/quote-requested.svelte-248181ae.js", "/dtc-website/_app/chunks/vendor-2f7a998b.js", "/dtc-website/_app/chunks/utils-3a7b53c0.js"], "styles": null }, "src/routes/services.svelte": { "entry": "/dtc-website/_app/pages/services.svelte-162befa1.js", "css": [], "js": ["/dtc-website/_app/pages/services.svelte-162befa1.js", "/dtc-website/_app/chunks/vendor-2f7a998b.js", "/dtc-website/_app/chunks/services-eeb3c769.js", "/dtc-website/_app/chunks/people-c7c3e1c6.js", "/dtc-website/_app/chunks/Checkbox-22d98cfa.js", "/dtc-website/_app/chunks/LinkButton-d31108c8.js"], "styles": null }, "src/routes/about.svelte": { "entry": "/dtc-website/_app/pages/about.svelte-bab485b4.js", "css": [], "js": ["/dtc-website/_app/pages/about.svelte-bab485b4.js", "/dtc-website/_app/chunks/vendor-2f7a998b.js", "/dtc-website/_app/chunks/people-c7c3e1c6.js"], "styles": null }, "src/routes/quote.svelte": { "entry": "/dtc-website/_app/pages/quote.svelte-4b7e7909.js", "css": [], "js": ["/dtc-website/_app/pages/quote.svelte-4b7e7909.js", "/dtc-website/_app/chunks/vendor-2f7a998b.js", "/dtc-website/_app/chunks/singletons-bb9012b7.js", "/dtc-website/_app/chunks/services-eeb3c769.js", "/dtc-website/_app/chunks/people-c7c3e1c6.js", "/dtc-website/_app/chunks/Checkbox-22d98cfa.js", "/dtc-website/_app/chunks/LinkButton-d31108c8.js", "/dtc-website/_app/chunks/utils-3a7b53c0.js"], "styles": null } };
+const metadata_lookup = { "src/routes/__layout.svelte": { "entry": "/./_app/pages/__layout.svelte-fd551ceb.js", "css": ["/./_app/assets/pages/__layout.svelte-2c13b87e.css"], "js": ["/./_app/pages/__layout.svelte-fd551ceb.js", "/./_app/chunks/vendor-2f7a998b.js", "/./_app/chunks/links-1a771544.js", "/./_app/chunks/LinkButton-d31108c8.js"], "styles": null }, ".svelte-kit/build/components/error.svelte": { "entry": "/./_app/error.svelte-e9da64e0.js", "css": [], "js": ["/./_app/error.svelte-e9da64e0.js", "/./_app/chunks/vendor-2f7a998b.js"], "styles": null }, "src/routes/index.svelte": { "entry": "/./_app/pages/index.svelte-f41b6e2b.js", "css": [], "js": ["/./_app/pages/index.svelte-f41b6e2b.js", "/./_app/chunks/vendor-2f7a998b.js", "/./_app/chunks/LinkButton-d31108c8.js", "/./_app/chunks/links-1a771544.js", "/./_app/chunks/services-eeb3c769.js", "/./_app/chunks/people-c7c3e1c6.js"], "styles": null }, "src/routes/quote-requested.svelte": { "entry": "/./_app/pages/quote-requested.svelte-248181ae.js", "css": [], "js": ["/./_app/pages/quote-requested.svelte-248181ae.js", "/./_app/chunks/vendor-2f7a998b.js", "/./_app/chunks/utils-3a7b53c0.js"], "styles": null }, "src/routes/services.svelte": { "entry": "/./_app/pages/services.svelte-162befa1.js", "css": [], "js": ["/./_app/pages/services.svelte-162befa1.js", "/./_app/chunks/vendor-2f7a998b.js", "/./_app/chunks/services-eeb3c769.js", "/./_app/chunks/people-c7c3e1c6.js", "/./_app/chunks/Checkbox-22d98cfa.js", "/./_app/chunks/LinkButton-d31108c8.js"], "styles": null }, "src/routes/about.svelte": { "entry": "/./_app/pages/about.svelte-bab485b4.js", "css": [], "js": ["/./_app/pages/about.svelte-bab485b4.js", "/./_app/chunks/vendor-2f7a998b.js", "/./_app/chunks/people-c7c3e1c6.js"], "styles": null }, "src/routes/quote.svelte": { "entry": "/./_app/pages/quote.svelte-4b7e7909.js", "css": [], "js": ["/./_app/pages/quote.svelte-4b7e7909.js", "/./_app/chunks/vendor-2f7a998b.js", "/./_app/chunks/singletons-bb9012b7.js", "/./_app/chunks/services-eeb3c769.js", "/./_app/chunks/people-c7c3e1c6.js", "/./_app/chunks/Checkbox-22d98cfa.js", "/./_app/chunks/LinkButton-d31108c8.js", "/./_app/chunks/utils-3a7b53c0.js"], "styles": null } };
 async function load_component(file) {
   return {
     module: await module_lookup[file](),
